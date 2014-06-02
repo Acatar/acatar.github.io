1bc1c50405d33deb7b121bd244115d80306e3cde
Andesy <andes@Andrews-MacBook-Pro.local>
2014-06-02 07:41:07 -0400

    adds source and cleaner startup

--- hilary/gettingStarted/scripts/compositionRoot.js
+++ hilary/gettingStarted/scripts/compositionRoot.js
@@ -1,39 +1,51 @@
-// hilary.use lets us pass in window dependencies, similar to the standard module pattern, 
-// except that the dependencies and the function arguments that they resolve to are right next to each other
-// the first argument is always the container, then the window dependencies are passed in, in order
-// it's totally optional => you could use the standar module pattern, require.js, whatever you want.
-hilary.use([hilary, jQuery, ko], function(cntr, hilary, $, ko) {
+// hilary.use lets us pass in window dependencies, similar to the standard 
+// module pattern, except that the dependencies and the function arguments 
+// that they resolve to are right next to each other. 
+//
+// The first argument is always the container, then the window dependencies 
+// are passed in, in order.
+//
+// hilary.use is optional. You could use a standard module pattern, 
+// require.js, or whatever you want.
+
+hilary.use([hilary, jQuery, ko, window], function(hilarysInnerContainer, hilary, $, ko, window) {
 	"use strict";
 
 	var _mockAjax, _controller;
 
-	// resolve a mock jQuery ajax service
-	// we also inject jQuery into the mock-ajax constructor (the first argument), 
-	// as well as an array of users (which will be used to resolve the promise), and 40 which is the number 
-	// of milliseconds we want the promise to wait before resolving
+	// Resolve a mock jQuery ajax service.
+	// We inject the "mock-ajax" service with a jQuery instance. Then we 
+	// make a fake promise that will be used instead of real ajax.
 	_mockAjax = hilary.resolve('mock-ajax')
 		.call(null, $)
 		.makePromise([{ id: 1, name: 'Hilary Page' }, { id: 2, name: 'Ole Kirk Kristiansen' }], 40);
 
-	// create a model factory that resolves myModel and injects knockout into it, when called
+	// create a model factory that resolves myModel and injects knockout 
+	// into it, when called
 	hilary.register('myModel', function() {
 		return hilary.resolve('myModelCtor')
 			(ko);
 	});
 
-	// create a view factory that resolves myView and injects jQuery and knockout into it, when called
+	// create a view factory that resolves myView and injects jQuery and 
+	// knockout into it, when called
 	hilary.register('myView', function() {
 		return hilary.resolve('myViewCtor')
 			($, ko);
 	}); 
 
-	// resolve myController and inject our _mockAjax singleton, model factory and view factory into it
+	// Resolve myController and inject our _mockAjax singleton, 
+	// an instance of the model factory and an instance of the 
+	// view factory into it.
+	// In a non-test scenario, we would inject $.ajax instead of _mockAjax
 	_controller = hilary.resolve('myController')
 		.call(null, _mockAjax, 
 			hilary.resolve('myModel').call(), 
 			hilary.resolve('myView').call());
 
-	// execute the controller
-	// this would not normally be part of composition root, as the controller would be chosen by a route engine
-	_controller.view();
+	window.example = {
+		load: function() {
+			_controller.view();
+		}
+	};	
 });
--- hilary/gettingStartedWithRequire/scripts/compositionRoot.js
+++ hilary/gettingStartedWithRequire/scripts/compositionRoot.js
@@ -1,18 +1,31 @@
-require(['hilary', 'mock-ajax', 'model', 'view', 'controller'], 
-	function(hilary, mockAjax, myModel, myView, myController) {
+// Using AMD, our modules registered with some of their dependencies already 
+// resolved. That's a bit of mix-and-match, and you could avoid it by only 
+// defining factories. Personally, I would go that route, but this example 
+// assumes you are already steeped in AMD, so I'm going for a realistic scenario.
+
+define('compositionRoot', ['hilary', 'mock-ajax', 'model', 'view', 'controller'], 
+	function(hilary /*, mockAjax, myModel, myView, myController */) {
 		"use strict";
 
 		var _mockAjax, _model, _view, _controller;
 
 		// resolve a mock jQuery ajax service
+		// jQuery was already injected into it by require.js
 		_mockAjax = hilary.resolve('mock-ajax')
 			.makePromise([{ id: 1, name: 'Hilary Page' }, { id: 2, name: 'Ole Kirk Kristiansen' }], 40);
 
-		// resolve myController and inject our _mockAjax singleton, model factory and view factory
+		// resolve myController and inject our _mockAjax singleton, 
+		// model factory and view factory into it. In a non-test 
+		// scenario, we would inject $.ajax instead of _mockAjax.
 		_controller = hilary.resolve('myController')
-			(_mockAjax, hilary.resolve('myModel').call(), hilary.resolve('myView').call());
+			.call(null, 
+				_mockAjax, 
+				hilary.resolve('myModel').call(), 
+				hilary.resolve('myView').call());
 
-		// execute the controller
-		// this would not normally be part of composition root, as the controller would be chosen by a route engine
-		_controller.view();
+		return {
+			start: function() {
+				_controller.view();
+			}
+		};
 	});
